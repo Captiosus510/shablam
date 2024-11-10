997de9dc9aca430e933635fc1e9fa78af43e1981
Dinara Shukayeva <dinara@MacBook-Air-513.local>
2024-11-10 00:02:25 -0700

    app.py and index.html pretty :)

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ upload_status = None
 # Route to handle file upload
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+    #dictionary with movie descriptions!
+    movie_descriptions = {
+        "Inception": ["Rating: 8.8/10", "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."], 
+        "Your Name": ["Rating: 8.4/10", "Two teenagers share a profound, magical connection upon discovering they are swapping bodies. Things manage to become even more complicated when the boy and girl decide to meet in person."], 
+        "Ironman": ["Rating: 7.9/10", "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil."]
+    }
+    
+
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
@@ -39,12 +48,12 @@ def index():
             # movie_name = find_best_movie(filepath)
             movie_name = ""
 
-            upload_status = 'success'
+            upload_status = True
             
             # Pass the result to the template
-            return render_template('index.html', movie_name=movie_name, upload_status=upload_status)
+            return render_template('index.html', movie_name=movie_name, movie_descriptions=movie_descriptions, upload_status=upload_status)
     
-    upload_status = 'failure'
+    upload_status = False
     return render_template('index.html', upload_status=upload_status)
 
 
